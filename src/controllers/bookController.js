fd9b2ddfbac8a2d2fd19eb2adad777f316ac861e
Sajeel Ahmad <sajeel.ahmed@protonmail.com>
2023-07-01 16:01:08 +0500

    :new:

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,6 +1,7 @@
-const books = require("../data/books.json");
+const { getAllBooks } = require("../services/bookService");
 
 // Get all books
 exports.getAllBooks = (req, res) => {
+  const books = getAllBooks();
   res.json(books);
 };
